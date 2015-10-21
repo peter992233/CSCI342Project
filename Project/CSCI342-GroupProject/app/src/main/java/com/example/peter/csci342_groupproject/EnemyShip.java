23e4e57c15e58cd4b4f86380e7e7f78637537940
pudgimus <pudgimusprime@Pudgimus-Primes-MacBook-Pro.local>
2015-10-21 17:17:13 +1100

    remake 4

---
+++ Project/CSCI342-GroupProject/app/src/main/java/com/example/peter/csci342_groupproject/EnemyShip.java
@@ -0,0 +1,205 @@
+package com.example.peter.csci342_groupproject;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.RectF;
+
+import java.util.Random;
+
+/**
+ * Created by pb864 on 17/10/15.
+ */
+public class EnemyShip {
+
+    //The Containing rect data for the enemy Ship
+    RectF rect;
+
+    //Random Shooting Generator (To Choose when the Enemy Should Shoot)
+    Random shootGen = new Random();
+
+    //Enemy Bitmap Data
+    private Bitmap enemyBMP;
+
+    //length and height data
+    private float length;
+    private float height;
+
+    //The far left X of the Enemy
+    private float x;
+
+    //The top Y coordinate of the Enemy
+    private float y;
+
+    //This is the Movement speed of the enemy ship class
+    private float shipSpeed;
+
+    //Movement types for the enemy
+    public final int EMPTY = 0;
+    public final int LEFT = 1;
+    public final int RIGHT = 2;
+    public final int DOWN = 3;
+    public final int UP = 4;
+
+    //The Dual Coordinate Movement allowing for movement on Diagonals as well as XY
+    private int[] shipMoving = {EMPTY,DOWN};
+
+    //Is the Enemy visible
+    boolean isVisible;
+
+
+
+    public EnemyShip(Context context, int pushX, int pushY, int screenX, int screenY) {
+
+        // Initialize a blank RectF
+        rect = new RectF();
+
+        //Set the length of the Ship
+        length = screenX / 20;
+
+        //Set the ship to display as visible
+        isVisible = true;
+
+        //Push X is a variable to push the ships position to a point along the XY axis
+        x = pushX * (length);
+
+        // Initialize the bitmap
+        //enemyBMP = BitmapFactory.decodeResource(context.getResources(), R.drawable.X);
+
+        // stretch the first bitmap to a size appropriate for the screen resolution
+        enemyBMP = Bitmap.createScaledBitmap(enemyBMP, (int) (length), (int) (height), false);
+
+        //Set The Enemy Movespeed
+        shipSpeed = 80;
+    }
+
+    public void update(long fps){
+
+        //Movement Left, Right, Up and Down Movement
+        if(shipMoving[0] == LEFT){
+            x = x - shipSpeed / fps;
+        }
+
+        if(shipMoving[0] == RIGHT){
+            x = x + shipSpeed / fps;
+        }
+
+        if(shipMoving[1] == UP){
+            y = y - shipSpeed / fps;
+        }
+
+        if(shipMoving[1] == DOWN){
+            y = y + shipSpeed / fps;
+        }
+
+        // Update rect which is used to detect hits
+        rect.top = y;
+        rect.bottom = y + height;
+        rect.left = x;
+        rect.right = x + length;
+
+    }
+
+
+
+    public boolean shouldShoot(float playerShipX, float playerShipLength){
+
+        int randomNumber = -1;
+
+        // If near the player run a random generator with a 1/100 chance to shoot
+        // This takes into account the large amount of updates being sent through for the enemy
+        if((playerShipX + playerShipLength > x && playerShipX + playerShipLength < x + length) ||
+                (playerShipX > x && playerShipX < x + length)) {
+
+            //The random Gen with 1 in 100 chance to shoot
+            randomNumber = shootGen.nextInt(100);
+            if(randomNumber == 0) {
+                return true;
+            }
+
+        }
+
+        //Fire Randomly regardless of player (1/1500)
+        randomNumber = shootGen.nextInt(1500);
+        if(randomNumber == 0){
+            return true;
+        }
+
+        return false;
+    }
+
+
+    // - - - - - - - Getters & Setters - - - - - - -
+
+    public RectF getRect() {
+        return rect;
+    }
+
+    public void setRect(RectF rect) {
+        this.rect = rect;
+    }
+
+    public Bitmap getEnemyBMP() {
+        return enemyBMP;
+    }
+
+    public void setEnemyBMP(Bitmap enemyBMP) {
+        this.enemyBMP = enemyBMP;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getShipSpeed() {
+        return shipSpeed;
+    }
+
+    public void setShipSpeed(float shipSpeed) {
+        this.shipSpeed = shipSpeed;
+    }
+
+    public int[] getShipMoving() {
+        return shipMoving;
+    }
+
+    public void setShipMoving(int[] shipMoving) {
+        this.shipMoving = shipMoving;
+    }
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void setIsVisible(boolean isVisible) {
+        this.isVisible = isVisible;
+    }
+}
---
+++ Project/CSCI342-GroupProject/app/src/main/java/com/example/peter/csci342_groupproject/GameView.java
@@ -0,0 +1,312 @@
+package com.example.peter.csci342_groupproject;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.v4.view.MotionEventCompat;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by pb864 on 17/10/15.
+ */
+public class GameView extends SurfaceView implements Runnable {
+
+
+
+    Context context;
+
+    //This is the game thread
+    private Thread gameThread = null;
+
+    //Locks the surface on which to draw graphics
+    private SurfaceHolder ourHolder;
+
+    //Boolean to set/unset whether the game is running
+    private volatile boolean playing;
+
+    //Set the default mode to be paused at the start
+    private boolean paused = true;
+
+    //Canvas and paint object for drawing
+    private Canvas canvas;
+    private Paint paint;
+
+    //FPS Rate and the Variable tacking how many frames have passed
+    private long fps;
+    private long timeThisFrame;
+
+    //Screen Dimensions
+    private int screenX, screenY;
+
+    private PlayerShip pShip;
+
+
+    private Projectile projectile;
+    private ArrayList<Projectile> playerBullets = new ArrayList<Projectile>();
+    private int maxPlayerProjectiles = 10; //Maximum number of projectiles the player can shoot
+    private boolean shotTap = false; //Has the last tap fired a shot
+    /*
+    private Projectile[] enemyBullets = new Projectile[200];
+    private int NextProjectile;
+    private int MaxProjectile = 10;
+    */
+
+
+    EnemyShip[] enemies = new EnemyShip[60];
+    int numEnemies = 0;
+
+    int score = 0;
+    private int lives = 0;
+
+
+
+    public GameView(Context context,int x, int y) {
+        super(context);
+
+        this.context = context;
+
+        ourHolder = getHolder();
+        paint = new Paint();
+
+        screenX = x;
+        screenY = y;
+
+
+        try{
+
+            //Load Sound Files
+
+        }catch(Exception e){
+            Log.d("CERROR", "GameView Contructor");
+            e.printStackTrace();
+
+        }
+
+
+        prepareLevel();
+    }
+
+
+
+    private void prepareLevel(){
+
+        //Init game Objects
+
+        //Make a Player Ship
+        pShip = new PlayerShip(context,screenX,screenY);
+
+        //Prepare Bullets
+
+        projectile = new Projectile(context, screenY);
+
+        //Init bullet Array
+        //Build Enemy array
+
+    }
+
+
+    @Override
+    public void run() {
+
+
+        while(playing){
+            long startFrameTime = System.currentTimeMillis();
+
+            //If not pause the game should update
+            if(!paused){
+                update();
+            }
+
+            //Draw the Frame
+            draw();
+
+
+            //Update the elapsed time
+            timeThisFrame = System.currentTimeMillis() - startFrameTime;
+            if(timeThisFrame >= 1){
+                fps = 1000/timeThisFrame;
+            }
+
+
+        }
+
+
+
+    }
+
+
+    private void update(){
+
+        boolean bumped = false;
+        boolean lost = false;
+
+
+        //Move the player ship
+        pShip.update(fps);
+
+        //Update the Enemies
+
+        //Update the Bullets
+        for(Projectile p : playerBullets ) {
+            if (p.getStatus()) {
+                p.update(fps);
+            }
+        }
+
+        //Check Collision
+
+        if(lost){
+            prepareLevel();
+        }
+
+        //Update player bullets
+        //Check the player bullet collisions (top & bottom of Screen + enemies)
+        //Check the enemies bullet collisions (top & bottom of Screen + player)
+
+    }
+
+
+    private void draw(){
+
+        if(ourHolder.getSurface().isValid()){
+
+            canvas = ourHolder.lockCanvas();
+
+            canvas.drawColor(Color.argb(255, 26, 128, 182));
+            paint.setColor(Color.argb(255, 255, 255, 255));
+
+            //Draw the Active Bullets
+
+            for(Projectile p : playerBullets ) {
+                if(p.getStatus()){
+                    canvas.drawRect(p.getRect(),paint);
+                    canvas.drawBitmap(p.getBmp(), p.getX(),p.getY(), paint);
+                }
+            }
+
+            //Draw the Player Ship
+            canvas.drawBitmap(pShip.getBmp(), pShip.getX(),screenY-(pShip.getHeight()*2),paint);
+
+            //Draw the Enemies
+
+            //Draw Score & Lives
+
+            paint.setColor(Color.argb(255,249,129,0));
+
+            paint.setTextSize(40);
+            canvas.drawText("Score: " + score + "\t\t\tLives: " + lives, 10 , 50 , paint);
+
+            ourHolder.unlockCanvasAndPost(canvas);
+
+
+
+        }
+
+    }
+
+
+    public void Pause(){
+
+        playing = false;
+        try{
+            gameThread.join();
+        }catch(Exception e){
+            Log.d("CERROR","Error Pause()");
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    public void Resume(){
+        playing = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent){
+
+
+            switch(motionEvent.getAction() & MotionEvent.ACTION_MASK){
+
+
+                case MotionEvent.ACTION_DOWN:{
+                    paused = false;
+                    pShip.setMovementState(pShip.STOPPED);
+                    //If the touch is in the top 7/8ths of the screen it is counted as movement
+                    if (motionEvent.getY() > screenY - screenY / 4) {
+                        if (motionEvent.getX() > screenX / 2) {
+                            pShip.setMovementState(pShip.RIGHT);
+                        } else {
+                            pShip.setMovementState(pShip.LEFT);
+                        }
+                    }
+
+                    if(shotTap == false) {
+                        if (motionEvent.getY() < screenY - screenY / 4) {
+                            if (projectile.shoot(pShip.getX() + pShip.getWidth() / 2, screenY, projectile.UP));
+
+                            shotTap = true;
+                            playerBullets.add(projectile);
+                            projectile = new Projectile(context, screenY); // reset projectile
+                        }
+                    }
+
+
+                    break;
+                }
+
+                case MotionEvent.ACTION_POINTER_DOWN:{
+                    int index = motionEvent.getActionIndex();
+                    int xPos = (int) MotionEventCompat.getX(motionEvent, index);
+                    int yPos = (int) MotionEventCompat.getY(motionEvent, index);
+
+
+                    paused = false;
+                    pShip.setMovementState(pShip.STOPPED);
+                    //If the touch is in the top 7/8ths of the screen it is counted as movement
+                    if (yPos > screenY - screenY / 4) {
+                        if (xPos > screenX / 2) {
+                            pShip.setMovementState(pShip.RIGHT);
+                        } else {
+                            pShip.setMovementState(pShip.LEFT);
+                        }
+                    }
+
+                    if(shotTap == false) {
+                        if (motionEvent.getY() < screenY - screenY / 4) {
+                            if (projectile.shoot(pShip.getX() + pShip.getWidth() / 2, screenY, projectile.UP));
+
+                            shotTap = true;
+                            playerBullets.add(projectile);
+                            projectile = new Projectile(context, screenY); // reset projectile
+                        }
+                    }
+                    break;
+                }
+
+
+                case MotionEvent.ACTION_UP:{
+                    paused = false;
+                    pShip.setMovementState(pShip.STOPPED);
+                    shotTap = false;
+                    break;
+                }
+
+
+        }
+        return true;
+    }
+
+}
